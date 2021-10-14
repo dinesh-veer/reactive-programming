fa63598562ab43f9091660c5663fa2d8649287f0
dineshveer <dveer123@hotmail.com>
2021-10-14 12:34:23 +0530

    error with observable

---
+++ rxjava-samples/src/main/java/rxjava_samples/rxjava/observableErrorThrowing/ObservableErrorExample.java
@@ -0,0 +1,37 @@
+package rxjava_samples.rxjava.observableErrorThrowing;
+
+import io.reactivex.Observable;
+
+public class ObservableErrorExample {
+
+	public static void main(String[] args) {
+	
+		System.out.println("----------------------");
+		System.out.println("Observable with error return same exception instance");
+		observableSameExceptioninstanceExample();
+		System.out.println("----------------------");
+		System.out.println("----------------------");
+		System.out.println("Observable with error returns new exception instance");
+		observableNewExceptioninstanceExample();
+		System.out.println("----------------------");
+
+	}
+	
+	/**
+	 *  observers gets the same exception instance 
+	 */
+	private static void observableSameExceptioninstanceExample() {
+	
+		Observable observable = Observable.error(new Exception("Manual exception is thrown"));
+		
+		observable.subscribe(System.out::println,exception->{System.out.println("first subscription hashcode value  =" +exception.hashCode());});
+		observable.subscribe(System.out::println,exception->{System.out.println("second subscription hashcode value  =" +exception.hashCode());});
+ 		
+	}
+
+	private static void observableNewExceptioninstanceExample() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
